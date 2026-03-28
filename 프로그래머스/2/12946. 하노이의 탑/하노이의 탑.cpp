659071ede451b36c4959247249e0ed29a2a9b5f1
김대중 <117511142+decfour@users.noreply.github.com>
2026-03-28 21:35:05 +0900

    [level 2] Title: 하노이의 탑, Time: 27.58 ms, Memory: 29.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> answer;
+
+void hanoi(int n, int from, int temp, int to) {
+    if (n == 1) {
+        answer.push_back({from, to});
+        return;
+    }
+    hanoi(n - 1, from, to, temp);
+    answer.push_back({from, to});
+    hanoi(n - 1, temp, from, to);
+}
+
+vector<vector<int>> solution(int n) {
+    hanoi(n, 1, 2, 3);
+    return answer;
+}
